dda7dfbf98873080140125294577aa03bf11aa9d
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2025-03-12 17:00:48 +0700

    ✨ Adding GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-5JS5SXW7" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
