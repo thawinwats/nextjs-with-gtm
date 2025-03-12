6734c4b840266b49a34eaa8f90b7bc64416e1c29
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2025-03-12 18:43:42 +0700

    ✨ Adding sendGTMEvent

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,3 +1,4 @@
+import { sendGTMEvent } from '@next/third-parties/google'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,7 +6,8 @@ export default function Docs() {
   return (
     <div className="grid place-content-center min-h-screen">
       <Link
-            href="/"
+        href="/"
+        onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'go-back' })}
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
           >Go back</Link>
     </div>
